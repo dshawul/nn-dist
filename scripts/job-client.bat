fb2f7c48be22450ae2e63095de69840a3eed90cb
Daniel Shawul <dshawul@yahoo.com>
2019-07-29 00:09:20 -0600

    Temporarily comment out multi-gpu job in windows.

--- scripts/job-client.bat
+++ scripts/job-client.bat
@@ -34,24 +34,24 @@ SET SCOPT=nn_type 0 reuse_tree 0 fpu_is_loss 0 fpu_red 0 cpuct_init %CPUCT% ^
           policy_temp %POL_TEMP% noise_frac %NOISE_FRAC%
 
 :rungames
-    if %GPUS% EQU 0 (
+REM    if %GPUS% EQU 0 (
         CALL %SC%\%EXE% nn_path %NDIR% %SCOPT% new sv %SV% ^
              pvstyle 1 selfplayp %~1 games0.pgn train0.epd quit
-    ) else (
-        SET /a I=%CPUS%/%GPUS%
-        DEL *.pid
-        for /L %%k IN (1,1,%GPUS%) DO START ((
-            echo %%k > %%k.pid
-            SET /a m=%%k-1
-            SET CUDA_VISIBLE_DEVICES=%%k
-            CALL %SC%\%EXE% nn_path %NDIR% %SCOPT% new sv %SV% ^
-                 pvstyle 1 selfplayp %~1 games%m%.pgn train%m%.epd quit
-            DEL %%k.pid
-        )
-        REM wait for processes to end
-        :wait
-        IF EXIST *.pid goto wait
-    )
+REM    ) else (
+REM        SET /a I=%CPUS%/%GPUS%
+REM        DEL *.pid
+REM        for /L %%k IN (1,1,%GPUS%) DO START (
+REM            echo %%k > %%k.pid
+REM            SET /a m=%%k-1
+REM            SET CUDA_VISIBLE_DEVICES=%%k
+REM            CALL %SC%\%EXE% nn_path %NDIR% %SCOPT% new sv %SV% ^
+REM                 pvstyle 1 selfplayp %~1 games%m%.pgn train%m%.epd quit
+REM            DEL %%k.pid
+REM        )
+REM        REM wait for processes to end
+REM        :wait
+REM        IF EXIST *.pid goto wait
+REM    )
     echo "All jobs finished"
 EXIT /B 0
 
